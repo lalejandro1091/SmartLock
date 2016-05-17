b7985ebe394614b485bcbeff788499cb5ca2336f
lalejandro1091 <lalejandro1091@hotmail.com>
2016-05-17 18:24:17 -0500

    con 2 luces

--- SmartLock/app/src/main/java/com/example/chiruza/smartlock/Luces.java
+++ SmartLock/app/src/main/java/com/example/chiruza/smartlock/Luces.java
@@ -19,7 +19,7 @@ public class Luces extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url1 = "http://luces.tunesources.com/php/apagarLuces.php";
+                String url1 = "http://luces.tunesources.com/php/apagarLuz1.php";
                 PostResponseAsyncTask task1 = new PostResponseAsyncTask(Luces.this, new AsyncResponse() {
                     @Override
                     public void processFinish(String s) {
@@ -35,7 +35,37 @@ public class Luces extends AppCompatActivity {
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url1 = "http://luces.tunesource.com/php/prenderLuces.php";
+                String url1 = "http://luces.tunesource.com/php/prenderLuz1.php";
+                PostResponseAsyncTask task2 = new PostResponseAsyncTask(Luces.this, new AsyncResponse() {
+                    @Override
+                    public void processFinish(String s) {
+                        s = s.substring(0, 2);
+                        Toast.makeText(Luces.this, s, Toast.LENGTH_LONG).show();
+                    }
+                });
+                task2.execute(url1);
+            }
+        });
+        Button btn3 = (Button) findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url1 = "http://luces.tunesource.com/php/prenderLuz2.php";
+                PostResponseAsyncTask task2 = new PostResponseAsyncTask(Luces.this, new AsyncResponse() {
+                    @Override
+                    public void processFinish(String s) {
+                        s = s.substring(0, 2);
+                        Toast.makeText(Luces.this, s, Toast.LENGTH_LONG).show();
+                    }
+                });
+                task2.execute(url1);
+            }
+        });
+        Button btn4 = (Button) findViewById(R.id.btn4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url1 = "http://luces.tunesource.com/php/prenderLuz2.php";
                 PostResponseAsyncTask task2 = new PostResponseAsyncTask(Luces.this, new AsyncResponse() {
                     @Override
                     public void processFinish(String s) {
